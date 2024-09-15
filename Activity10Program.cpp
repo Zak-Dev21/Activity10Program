2f27e65c4c02a17f4d807a88ac9cc1844c40ce8f
Zak <zakariabouaddou@gmail.com>
2024-09-15 12:51:28 -0700

    changing if condition to properly check for range between values.

--- Activity10Program.cpp
+++ Activity10Program.cpp
@@ -5,10 +5,10 @@ using namespace std;
 
 int main()
 {
-    int sales = 5'000;
+    int sales = 1'000;
 
     double commision = (sales > 15'000) ? .2 :
-        (10'000 <= sales <= 15'000) ? .15 : .1;
+        (sales >= 10'000 && sales <= 15'000) ? .15 : .1;
 
     cout << "The commision is: " << commision << endl;
 }
