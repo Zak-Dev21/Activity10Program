634233755563f9abf1d865caac253b8c422661a7
Zak <zakariabouaddou@gmail.com>
2024-09-15 12:52:53 -0700

    adjusted indent for cleaner code.

--- Activity10Program.cpp
+++ Activity10Program.cpp
@@ -8,7 +8,7 @@ int main()
     int sales = 1'000;
 
     double commision = (sales > 15'000) ? .2 :
-        (sales >= 10'000 && sales <= 15'000) ? .15 : .1;
+                       (sales >= 10'000 && sales <= 15'000) ? .15 : .1;
 
     cout << "The commision is: " << commision << endl;
 }
